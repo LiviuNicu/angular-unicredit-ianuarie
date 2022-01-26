2a22dcd42ea51dc6da3f1b7e2eb61b877ba6a5f3
Liviu <liviu@luxon-tech.com>
2022-01-26 15:59:26 +0200

    register form

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { LoginDTO } from 'src/app/interfaces/login-dto';
 
 @Component({
   selector: 'app-login',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   public isDisabled: boolean = false;
-  public user: any = {
+  public user: LoginDTO = {
     email: 'test@tes.com',
     password: '',
   };
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,15 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnChanges, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-
-  constructor() { }
+  public myForm!: FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.myForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      firstName: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(5)]],
+    });
   }
 
+  doRegister() {
+    console.log(this.myForm);
+  }
 }
